8e1d3604e3c23ff8ac24630344673ca65f69000a
Surbhi Saraogi <surbhi.saraogi@ETC02YQGBFLVCF.local>
2019-11-22 15:43:29 +0530

    Added code for paddle and ball creation , and paddle movement

---
+++ Ping_Pong.py
@@ -0,0 +1,72 @@
+import turtle
+
+game_window = turtle.Screen()
+game_window.title("Ping Pong by @Surbhi-1206")
+game_window.bgcolor("black")
+game_window.setup(width=800, height=600)
+game_window.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
+# function up
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# keyboard binding
+
+game_window.listen()
+game_window.onkeypress(paddle_a_up, "w")
+game_window.onkeypress(paddle_a_down, "s")
+game_window.onkeypress(paddle_b_up, "Up")
+game_window.onkeypress(paddle_b_down, "Down")
+
+# Main game loopwwww
+while True:
+    game_window.update()
